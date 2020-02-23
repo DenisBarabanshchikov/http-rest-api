a168cc69c34034977e7bf66ec0ba67852ae0b303
Denis Barabanshchikov <denisbarabansikov@iMac-Denis.fritz.box>
2020-02-23 11:29:53 +0100

    refactoring store

---
+++ internal/app/store/teststore/userrepository_test.go
@@ -0,0 +1,28 @@
+package teststore
+
+import (
+	"github.com/DenisBarabanshchikov/http-rest-api/internal/app/model"
+	"github.com/DenisBarabanshchikov/http-rest-api/internal/app/store"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s := New()
+	u := model.TestUser(t)
+
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s := New()
+	email := "user@example.org"
+	_, err := s.User().FindByEmail(email);
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
+
+	s.User().Create(model.TestUser(t))
+	u, err := s.User().FindByEmail(email);
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
